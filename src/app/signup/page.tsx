8e133456550b0bb8d5f44a2b046c1419389c0b1d
Piyush Raj <sciencetechoindia@gmail.com>
2025-09-12 18:49:28 +0530

    feat: enhance Login and Signup pages with form inputs and navigation links

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
-function Signup() {
+function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
   return (
-    <div>Signup</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        name=""
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="Username"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name=""
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name=""
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+        onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+        Signup
+      </button>
+      <Link href="./login">Visit Login Page</Link>
+    </div>
+  );
 }
 
-export default Signup
+export default SignupPage;
