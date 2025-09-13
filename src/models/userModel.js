562ea249c2c9fb8d74299401566ccfd1d81f2080
Piyush Raj <sciencetechoindia@gmail.com>
2025-09-13 18:43:38 +0530

    feat: implement user authentication with login and signup functionality, including JWT token generation and password hashing

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide an username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+export const User =
+  mongoose.models.users || mongoose.model("users", userSchema);
